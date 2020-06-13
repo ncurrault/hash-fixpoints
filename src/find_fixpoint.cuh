a896433ed6a520faf11772ba9870fa1286b12588
Nicholas Currault <nicholas.curr@gmail.com>
2020-06-13 00:26:32 -0700

    deleted old assignment-specific code

--- src/find_fixpoint.cuh
+++ src/find_fixpoint.cuh
@@ -1,32 +1,8 @@
-/* CUDA blur
- * Kevin Yuh, 2014 */
+#ifndef CUDA_FIND_FIXPOINT_CUH
+#define CUDA_FIND_FIXPOINT_CUH
 
-#ifndef CUDA_FFT_CONVOLVE_CUH
-#define CUDA_FFT_CONVOLVE_CUH
-
-
-
-#include <cufft.h>
-
-void cudaCallProdScaleKernel(const unsigned int blocks,
-        const unsigned int threadsPerBlock,
-        const cufftComplex *raw_data,
-        const cufftComplex *impulse_v,
-        cufftComplex *out_data,
-        const unsigned int padded_length);
-
-
-void cudaCallMaximumKernel(const unsigned int blocks,
-        const unsigned int threadsPerBlock,
-        cufftComplex *out_data,
-        float *max_abs_val,
-        const unsigned int padded_length);
-
-
-void cudaCallDivideKernel(const unsigned int blocks,
-        const unsigned int threadsPerBlock,
-        cufftComplex *out_data,
-        float *max_abs_val,
-        const unsigned int padded_length);
+// TODO
+// void cudaShaFixpointSearchKernel
+// void cudaTreeFixpointSearchKernel
 
 #endif
