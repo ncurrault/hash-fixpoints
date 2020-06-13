6e93f7e792cfdb446927b7a713ae1e3e87aa9a53
Nicholas Currault <nicholas.curr@gmail.com>
2020-06-13 01:43:33 -0700

    fix accidental double-declaration

--- src/find_fixpoint.cuh
+++ src/find_fixpoint.cuh
@@ -7,15 +7,11 @@
 #define PREFIX_COUNTER_TYPE uint32_t
 #define PREFIX_COUNTER_MAX UINT32_MAX
 
-uint32_t leftrotate(uint32_t a, uint32_t b);
-void sha1ofPrefix(uint8_t* result, uint8_t* prefix);
-
 union PrefixCounter {
     PREFIX_COUNTER_TYPE n;
     uint8_t prefix[PREFIX_LEN];
 };
 
-void cudaShaFixpointSearchKernel(bool* success, uint8_t* prefix);
 
 bool cudaCallShaFixpointSearchKernel(const unsigned int threads_per_block,
     const unsigned int blocks, uint8_t* resultDest);
